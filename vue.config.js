f35ebe06c9f7ade83c90a3b2bac65fe3fc86f008
赵雅国 <1248023357@qq.com>
2022-02-26 17:10:26 +0800

    feat(init config)

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+ module.exports = {
+  // 选项...
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue-color-avatar-test/'
+  : '/'
+};
